2b04cb5ac6f7e7130e10e93192c9359e06d37f87
sttng <46765778+sttng@users.noreply.github.com>
2020-10-28 13:39:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -38,6 +38,10 @@ async function run() {
 tfvis.show.modelSummary({name: 'Model Architecture'}, model);
   
 await train(model, data);
+  
+  await showAccuracy(model, data);
+await showConfusion(model, data);
+  
 }
 
 document.addEventListener('DOMContentLoaded', run);
